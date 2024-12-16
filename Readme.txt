fd7d93919125fc1969df2b8e09896e9518e00eea
samarth2534@gmail.com <samarth2534@gmail.com>
2024-12-16 18:08:47 +0530

    Search box and debouncing

--- Readme.txt
+++ Readme.txt
@@ -14,3 +14,22 @@
    - side bar - menu items
    - main container - buttonsList
    - video container - VideoCard
+
+
+
+
+- Debouncing -> are used to enhance browser performance by ensuring that expensive operations are executed only when neccessary.
+
+Performance: 
+  - iPhone pro max = 14 api calls
+  - with debouncing = 3 api calls
+  Ex: Flipkart - debouncing - 200ms ( if difference between 2 key strokes is less than 200 - decline the api call)
+                              >200ms => make an api call and show the results 
+
+      Implemenatation
+      - make an api call after every key press
+      - but if the difference between 2 API calls is <200ms
+      - decline the api call
+
+
+http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q=Query 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../store/appSlice";
-import { Link } from "react-router-dom";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    try {
+      console.log(searchQuery);
+      const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+      const json = await data.json();
+      setSuggestions(json[1]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
@@ -26,14 +50,27 @@ const Header = () => {
         />
       </div>
       <div className="col-span-10 px-20">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-          placeholder="Search"
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
-          🔍
-        </button>
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
+            🔍
+          </button>
+        </div>
+        <div className="fixed bg-white py-2 px-2 w-[30rem] shadow-lg rounded-lg border border-gray-100 ">
+          <ul>
+            {suggestions.map((s) => (
+              <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                🔍 {s}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="col-span-1">
         <img
